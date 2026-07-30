81fbb66ba3392697f98df4a5de9501ef70f5bf4c
chenjing <247345858@qq.com>
2026-07-30 09:24:55 +0800

    feat: 缩放时居中展示

---
+++ src/features/editor/hooks/use-auto-resize.ts
@@ -0,0 +1,72 @@
+import React, { useEffect, useCallback } from 'react';
+import { fabric } from 'fabric';
+interface useAutoResizeProps {
+  canvas: fabric.Canvas | null;
+  container: HTMLDivElement | null;
+}
+
+export const useAutoResize = ({ canvas, container }: useAutoResizeProps) => {
+  const autoZoom = useCallback(() => {
+    if (!canvas || !container) return;
+
+    const width = container.offsetWidth;
+    const height = container.offsetHeight;
+
+    canvas.setWidth(width);
+    canvas.setHeight(height);
+    
+
+    const center = canvas.getCenter()
+    const zoomRatio = 0.85
+    const localWorkspace = canvas.getObjects().find((object)=> object.name === 'clip' )
+
+    // @ts-expect-error findScaleToFit does not exist in type, but the method acutally do exist
+    const scale = fabric.util.findScaleToFit(localWorkspace, {
+      width, 
+      height, 
+    })
+
+    const zoom = zoomRatio * scale
+
+    canvas.setViewportTransform(fabric.iMatrix.concat())
+    canvas.zoomToPoint(new fabric.Point(center.left, center.top), zoom)
+
+    if(!localWorkspace) return;
+
+    const workspaceCenter = localWorkspace.getCenterPoint()
+    const viewportTransform = canvas.viewportTransform;
+
+    if(canvas.width === undefined || canvas.height === undefined || !viewportTransform){
+      return;
+    }
+
+    const nextViewportTransform = viewportTransform.concat();
+    nextViewportTransform[4] = canvas.width / 2 - workspaceCenter.x * nextViewportTransform[0]
+    nextViewportTransform[5] = canvas.height / 2 - workspaceCenter.y * nextViewportTransform[3]
+
+    canvas.setViewportTransform(nextViewportTransform)
+
+    localWorkspace.clone((cloned: fabric.Rect) => {
+      canvas.clipPath = cloned
+      canvas.requestRenderAll()
+    })
+  }, [canvas, container]);
+
+  useEffect(() => {
+    let resizeObserver: ResizeObserver | null = null;
+
+    if (canvas && container) {
+      resizeObserver = new ResizeObserver(() => {
+        autoZoom();
+      });
+
+      resizeObserver.observe(container);
+    }
+
+    return () => {
+      if (resizeObserver) {
+        resizeObserver.disconnect();
+      }
+    };
+  }, [canvas, container, autoZoom]);
+};
