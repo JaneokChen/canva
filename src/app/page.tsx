8be3beda6b385e9f07f37cb3887fe5f57ff65ca4
chenjing <247345858@qq.com>
2026-09-17 15:43:22 +0800

    feat: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 // import { Button } from "@/components/ui/button";
+import { Editor } from "@/features/editor/components/editor";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      {/* <Button size="sm" variant="destructive"> */}
-      Click me
-      {/* </Button> */}
-    </div>
-  );
+  return <Editor />;
 }
